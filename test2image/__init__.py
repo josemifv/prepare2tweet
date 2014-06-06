d514dc7d20b010f353e114367af050012611de3b
José Miguel Franco <josemiguel.franco@ciemat.es>
2014-06-06 20:38:41 +0200

    Project structure created
    Project structure created

---
+++ test2image/__init__.py
@@ -0,0 +1,14 @@
+
+"""
+    Text2Image
+    ~~~
+
+    A library to convert large text strings to image files
+
+
+    :copyright: (c) 2014 by José M. Franco-Valiente.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1-dev'
+
